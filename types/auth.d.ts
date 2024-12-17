f2ba73c49b59ca3ba6248c4c3d8021fea2a2c6e7
wangbo <99548050@qq.com>
2024-12-17 17:12:20 +0800

    set git tag

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -2,7 +2,7 @@ declare namespace Auth {
     interface LoginData {
         /** 用户名 */
         username: string;
-        /** 密码 */
+        /** 密码1111 */
         password: string;
     }
     interface AutoLoginRes {
