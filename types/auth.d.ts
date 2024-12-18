842ea1f5b3e4f29b69a988dc3d62abf37298f2cb
wangbo <99548050@qq.com>
2024-12-18 11:26:13 +0800

    ????

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -2,13 +2,21 @@
  * @Author: wangbo 99548050@qq.com
  * @Date: 2024-12-17 15:44:14
  * @LastEditors: wangbo 99548050@qq.com
+<<<<<<< HEAD
  * @LastEditTime: 2024-12-18 09:42:28
+=======
+ * @LastEditTime: 2024-12-18 10:09:32
+>>>>>>> 5d35022 (测试reabase 1)
  * @FilePath: \vue-element-admin\types\auth.d.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 declare namespace Auth {
     interface LoginData {
+<<<<<<< HEAD
         /** 用户名2221 */
+=======
+        /** 用户名2221abcde */
+>>>>>>> 5d35022 (测试reabase 1)
         username: string;
         /** 密码1111zzz */
         password: string;
